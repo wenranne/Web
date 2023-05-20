c28fbe8c9865af5b4eb67beb6688de644ce3d783
Ali <108894573+wenranne@users.noreply.github.com>
2023-05-20 18:59:06 +0200

    Add files via upload

---
+++ P1|SVG Usage/logic.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var circle = document.getElementById("myCircle");
+
+  circle.addEventListener("click", function() {
+    alert("Circle clicked!");
+  });
+});
